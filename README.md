d9ae34d3277988eb0876e9668d649c218e6de7aa
Pablo Machado <pablo@tiqets.com>
2020-09-26 11:58:46 +0200

    Implement basic features logic (no styles)
    Create, Delete, Random text, paste, focusout, count

---
+++ README.md
@@ -0,0 +1,3 @@
+Why roolup?
+OVERDOCUMENTATION
+given the amount of requirements and the time thta I had available I've decided the not too much effort into e-mail validation and random e-mail gerenation.
--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,116 @@ import "./style.css";
 
 const rootClassName = "EmailsInput";
 
-const EmailsInput = (node: HTMLElement) => {
-  // Using innerHTML to save time. Even though it ins't safe or fast.
-  node.innerHTML = `
-    <div>
-        <span>Share <b>Board Name</b> with others</span>
-        <ul>
-            <li>machadoum@gmail.com</li>
-            <input>
-        </ul>
-    </div>
-    <div>
-        <button>
-            Add email
-        </button>
-        <button>
-            Get emails count
-        </button>
-    </div>
-  `;
+const initialHtml = `
+<div>
+    <span>Share <b>Board Name</b> with others</span>
+    <ul><li><input type="email" placeholder="add more people..."></li></ul>
+</div>
+<div>
+    <button data-add-button>
+        Add email
+    </button>
+    <button data-add-count>
+        Get emails count
+    </button>
+</div>
+`;
 
-  node.classList.add("EmailsInput");
+const addNewTag = (
+  tagList: HTMLUListElement,
+  tagText: string,
+  validity: boolean
+) => {
+  const newTag = document.createElement("li");
+  // TODO create an element to be clicked
+  newTag.addEventListener("click", () => {
+    newTag.remove();
+  });
+  if (!validity) {
+    newTag.setAttribute("data-invalid", "");
+    newTag.classList.add("invalid");
+  }
+
+  newTag.innerText = tagText;
+  tagList.insertBefore(
+    newTag,
+    tagList.childNodes[tagList.childNodes.length - 1]
+  );
 };
+
+const clearInput = (input: HTMLInputElement) => {
+  input.value = "";
+};
+
+// It's a very simple and silly way to generate random text.
+const randomText = () => Math.random().toString(36).substring(7);
+
+const randomEmail = () => `${randomText()}@${randomText()}.com`;
+
+const EmailsInput = (node: HTMLElement) => {
+  // ---------- SETUP HTML INITIAL STATE ----------
+
+  // Using innerHTML to save time. Even though it ins't safe or fast.
+  node.classList.add(rootClassName);
+  node.innerHTML = initialHtml;
+
+  // ---------- QUERY SELECTORS  ----------
+
+  const input = node.querySelector("input")!;
+  const tagList = node.querySelector("ul")!;
+  const addButton = node.querySelector("button[data-add-button]")!;
+  const countButton = node.querySelector("button[data-add-count]")!;
+
+  // ---------- ATTACH EVENT LISTENER ----------
+
+  input.addEventListener("paste", (event: ClipboardEvent) => {
+    let pastedText = event.clipboardData!.getData("text");
+    pastedText
+      ?.trim()
+      .split(",")
+      .forEach((text) => {
+        if (text != "") {
+          input.value = text;
+          addNewTag(tagList, text, input.validity.valid);
+        }
+      });
+
+    clearInput(input);
+    event.preventDefault();
+  });
+
+  input.addEventListener("keyup", (event: KeyboardEvent) => {
+    const cleanValue = input.value.replace(/[,]/g, "");
+
+    if (["Enter", ","].includes(event.key)) {
+      if (cleanValue != "") {
+        addNewTag(tagList, cleanValue, input.validity.valid);
+      }
+      clearInput(input);
+    }
+  });
+
+  const addTagAndClearInput = (tagText: string) => {
+    addNewTag(tagList, tagText, input.validity.valid);
+    clearInput(input);
+  };
+
+  input.addEventListener(
+    "focusout",
+    () => input.value != "" && addTagAndClearInput(input.value)
+  );
+
+  addButton.addEventListener("click", () => {
+    addTagAndClearInput(randomEmail());
+  });
+
+  countButton.addEventListener("click", () => {
+    const invalidTags = [...tagList.children].filter(
+      (tag) => (tag as HTMLLIElement).getAttribute("data-invalid") === null
+    );
+
+    alert(invalidTags.length - 1); // Subtract one for the input field
+  });
+};
+
 export default EmailsInput;
