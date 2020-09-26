b0a72165816efbcb554ad29d6f1c76bc4477ee83
Pablo Machado <pablo@tiqets.com>
2020-09-26 10:47:10 +0200

    Create initial HTML structure
    Using innerHTML to save time. Even though it ins't safe or fast.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,27 @@
 import "./style.css";
 
-const EmailsInput = (node: HTMLElement) => console.log(node);
+const rootClassName = "EmailsInput";
+
+const EmailsInput = (node: HTMLElement) => {
+  // Using innerHTML to save time. Even though it ins't safe or fast.
+  node.innerHTML = `
+    <div>
+        <span>Share <b>Board Name</b> with others</span>
+        <ul>
+            <li>machadoum@gmail.com</li>
+            <input>
+        </ul>
+    </div>
+    <div>
+        <button>
+            Add email
+        </button>
+        <button>
+            Get emails count
+        </button>
+    </div>
+  `;
+
+  node.classList.add("EmailsInput");
+};
 export default EmailsInput;
